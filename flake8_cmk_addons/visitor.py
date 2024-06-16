520cb17e8e995ee70e8b367be99d3f8c0dda577f
Marius Rieder <marius.rieder@durchmesser.ch>
2024-06-16 20:27:21 +0200

    Add more checks

--- flake8_cmk_addons/visitor.py
+++ flake8_cmk_addons/visitor.py
@@ -13,11 +13,36 @@ def simplify_keywords(keywords):
 
 class CmkAddonsVisitor(Visitor):
     CALL_CHECKS = {
+        # cmk.agent_based.v2
         'cmk.agent_based.v2.AgentSection': 'AgentSection',
         'cmk.agent_based.v2.SimpleSNMPSection': 'SimpleSNMPSection',
         'cmk.agent_based.v2.SNMPSection': 'SNMPSection',
         'cmk.agent_based.v2.CheckPlugin': 'CheckPlugin',
         'cmk.agent_based.v2.InventoryPlugin': 'InventoryPlugin',
+        # cmk.graphing.v1
+        'cmk.graphing.v1.metrics.Metric': 'Metric',
+        'cmk.graphing.v1.translations.Translation': 'Translation',
+        'cmk.graphing.v1.perfometers.Perfometer': 'Perfometer',
+        'cmk.graphing.v1.perfometers.Bidirectional': 'Perfometer',
+        'cmk.graphing.v1.perfometers.Stacked': 'Perfometer',
+        'cmk.graphing.v1.graphs.Graph': 'Graph',
+        'cmk.graphing.v1.graphs.Bidirectional': 'Graph',
+        # cmk.rulesets.v1
+        'cmk.rulesets.v1.rule_specs.ActiveCheck': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.AgentConfig': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.AgentAccess': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.EnforcedService': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.CheckParameters': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.Host': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.InventoryParameters': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.NotificationParameters': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.DiscoveryParameters': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.Service': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.SNMP': 'RuleSpec',
+        'cmk.rulesets.v1.rule_specs.SpecialAgent': 'RuleSpec',
+        # cmk.server_side_calls.v1
+        'cmk.server_side_calls.v1.ActiveCheckConfig': 'ActiveCheckConfig',
+        'cmk.server_side_calls.v1.SpecialAgentConfig': 'SpecialAgentConfig',
     }
 
     def __init__(self, config: None = None) -> None:
@@ -63,6 +88,39 @@ class CmkAddonsVisitor(Visitor):
 
     def check_AgentSection(self, call, targets=None):
         self.check_entry_point(call, targets, 'AgentSection', 'agent_section_')
+
+    def check_RuleSpec(self, call, targets=None):
+        if isinstance(call.func, ast.Attribute):
+            func_name = call.func.attr
+        else:
+            func_name = call.func.id
+        self.check_entry_point(call, targets, func_name, 'rule_spec_')
+
+    def check_ActiveCheckConfig(self, call, targets=None):
+        self.check_entry_point(call, targets, 'ActiveCheckConfig', 'active_check_')
+
+    def check_SpecialAgentConfig(self, call, targets=None):
+        self.check_entry_point(call, targets, 'SpecialAgentConfig', 'special_agent_')
+
+    def check_Metric(self, call, targets=None):
+        self.check_entry_point(call, targets, 'Metric', 'metric_')
+
+    def check_Translation(self, call, targets=None):
+        self.check_entry_point(call, targets, 'Translation', 'translation_')
+
+    def check_Perfometer(self, call, targets=None):
+        if isinstance(call.func, ast.Attribute):
+            func_name = call.func.attr
+        else:
+            func_name = call.func.id
+        self.check_entry_point(call, targets, func_name, 'perfometer_')
+
+    def check_Graph(self, call, targets=None):
+        if isinstance(call.func, ast.Attribute):
+            func_name = call.func.attr
+        else:
+            func_name = call.func.id
+        self.check_entry_point(call, targets, func_name, 'graph_')
 
     def check_Call(self, call: ast.Call, targets=None):
         fqpn = self.get_fqpn(call.func)
--- tests/test_CA001_entry_point.py
+++ tests/test_CA001_entry_point.py
@@ -1,3 +1,4 @@
+import pytest
 from flake8_plugin_utils import assert_error, assert_not_error
 
 from flake8_cmk_addons.visitor import CmkAddonsVisitor
@@ -13,18 +14,36 @@ def test_ok():
     assert_not_error(CmkAddonsVisitor, code)
 
 
-def test_no_assignment():
-    code = """
-        from cmk.agent_based.v2 import AgentSection
-
-        AgentSection(name='test')
-    """
+@pytest.mark.parametrize('code, class_name, entry_point_prefix', [
+    ['cmk.graphing.v1.metrics.Metric(name="test")', 'Metric', 'metric_'],
+    ['cmk.graphing.v1.translations.Translation(name="test")', 'Translation', 'translation_'],
+    ['cmk.graphing.v1.perfometers.Perfometer(name="test")', 'Perfometer', 'perfometer_'],
+    ['cmk.graphing.v1.perfometers.Bidirectional(name="test")', 'Bidirectional', 'perfometer_'],
+    ['cmk.graphing.v1.perfometers.Stacked(name="test")', 'Stacked', 'perfometer_'],
+    ['cmk.graphing.v1.graphs.Graph(name="test")', 'Graph', 'graph_'],
+    ['cmk.graphing.v1.graphs.Bidirectional(name="test")', 'Bidirectional', 'graph_'],
+    ['cmk.rulesets.v1.rule_specs.ActiveCheck(name="test")', 'ActiveCheck', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.AgentConfig(name="test")', 'AgentConfig', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.AgentAccess(name="test")', 'AgentAccess', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.EnforcedService(name="test")', 'EnforcedService', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.CheckParameters(name="test")', 'CheckParameters', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.Host(name="test")', 'Host', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.InventoryParameters(name="test")', 'InventoryParameters', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.NotificationParameters(name="test")', 'NotificationParameters', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.DiscoveryParameters(name="test")', 'DiscoveryParameters', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.Service(name="test")', 'Service', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.SNMP(name="test")', 'SNMP', 'rule_spec_'],
+    ['cmk.rulesets.v1.rule_specs.SpecialAgent(name="test")', 'SpecialAgent', 'rule_spec_'],
+    ['cmk.server_side_calls.v1.ActiveCheckConfig(name="test")', 'ActiveCheckConfig', 'active_check_'],
+    ['cmk.server_side_calls.v1.SpecialAgentConfig(name="test")', 'SpecialAgentConfig', 'special_agent_'],
+])
+def test_no_assignment_rulesets(code, class_name, entry_point_prefix):
     assert_error(
         CmkAddonsVisitor,
         code,
         EntryPoint,
-        class_name='AgentSection',
-        entry_point_prefix='agent_section_',
+        class_name=class_name,
+        entry_point_prefix=entry_point_prefix,
         instance_name='test',
     )
 
